29101979003a21212f2c09c89e0f9a6a78b96c5e
Boi Schrader <boi1992@t-online.de>
2022-07-07 13:50:45 +0200

    done

--- solution.js
+++ solution.js
@@ -1,1 +1,27 @@
 
+var euroCities = ["Paris", "London", "Valletta", "Prague", "Rome"];
+var secondCity = euroCities[1];
+euroCities [0] = 'Berlin';
+console.log(euroCities, 'length is', euroCities.length);
+console.log('euroCIties length is', euroCities.length);
+euroCities.pop();
+euroCities.push('Budapaest')
+var asianCities =  ['Tokyo','Hanoi','Taipei','Hong Kong','Beijing']
+var worldCities = [...euroCities, ...asianCities]
+console.log(worldCities.reverse());
+euroCities.splice(2,1);
+console.log(euroCities);
+var removedAsianCities = asianCities.splice(1,3)
+console.log(removedAsianCities)
+
+
+worldCities[4]  = 'Toronto' ;
+worldCities.splice(1, 0, 'Washington');
+console.log(worldCities)
+console.log(worldCities.join())
+
+var message = "Hello World"
+console.log(message.split('').reverse().join(''))
+
+
+
