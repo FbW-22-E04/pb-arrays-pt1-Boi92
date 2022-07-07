311e4a5d60812b67c14adcfde97686d65fcddd63
Boi Schrader <boi1992@t-online.de>
2022-07-07 14:12:35 +0200

    autograding is making me angry

--- solution.js
+++ solution.js
@@ -1,24 +1,33 @@
 
 var euroCities = ["Paris", "London", "Valletta", "Prague", "Rome"];
+
 var secondCity = euroCities[1];
 euroCities [0] = 'Berlin';
 console.log(euroCities.length);
+
 euroCities.pop();
-euroCities.push('Budapaest')
+euroCities.push('Budapest')
 console.log(euroCities)
+
 var asianCities =  ['Tokyo','Hanoi','Taipei','Hong Kong','Beijing']
 console.log(asianCities)
+
 var worldCities = [...euroCities, ...asianCities]
-console.log(worldCities.reverse());
+console.log(worldCities.reverse())
+
 euroCities.splice(2,1);
 console.log(euroCities);
+
 var removedAsianCities = asianCities.splice(1,3)
-console.log(removedAsianCities)
+
+
+console.log(worldCities)
 
 
 worldCities[4]  = 'Toronto' ;
 worldCities.splice(1, 0, 'Washington');
 console.log(worldCities)
+
 console.log(worldCities.join())
 
 var message = "Hello World"
